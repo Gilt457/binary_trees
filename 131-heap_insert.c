9b263ecaa2da4306081597e237a4d911ab520837
Gilt457 <lepatioborel@gmail.com>
2024-02-28 09:24:17 -0800

    update

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -1,55 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * swap - swaps two nodes
- * @a: first node
- * @b: second node
- * Return: nothing
+ * node_swap - swaps two nodes
+ * @node: pointer to the node to be swapped
  */
-void swap(heap_t **a, heap_t **b)
+void node_swap(heap_t **node)
 {
-	(*a)->parent = (*b)->parent;
-	if ((*b)->parent)
+	int temp = (*node)->n;
+	while ((*node)->parent && (*node)->n > (*node)->parent->n)
 	{
-		if ((*b)->parent->left == *b)
-			(*b)->parent->left = *a;
-		else
-			(*b)->parent->right = *a;
-	}
-	(*b)->parent = *a;
-}
-
-/**
- * heapify - fix the heap_max
- * @root: head of the heap tree
- * Return: nothing
- */
-void heapify(heap_t **root)
-{
-	heap_t *tmp = *root, *node = NULL;
-
-	while (1)
-	{
-		if (tmp->left)
-		{
-			if (tmp->n < tmp->left->n)
-				node = tmp->left;
-			if (tmp->right && tmp->n < tmp->right->n && tmp->right->n > node->n)
-				node = tmp->right;
-		}
-		else if (tmp->right)
-		{
-			if (tmp->n < tmp->right->n)
-				node = tmp->right;
-		}
-		if (node)
-		{
-			swap(&node, &tmp);
-			if (!tmp->parent)
-				*root = tmp;
-		}
-		else
-			break;
+		(*node)->n = (*node)->parent->n;
+		(*node)->parent->n = temp;
+		*node = (*node)->parent;
 	}
 }
 
@@ -61,28 +23,56 @@ void heapify(heap_t **root)
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *node, *parent;
-	char *binary;
-	unsigned int idx, size;
+	heap_t *node;
+	size_t parent_size, node_size;
 
-	parent = *root;
-	node = binary_tree_node(parent, value);
+	node = binary_tree_node(NULL, value);
 	if (!node)
 		return (NULL);
 	if (!(*root))
-		return (*root = node);
-	for (size = 0; parent; size++)
-		parent = parent->parent;
-	for (idx = 0, binary = malloc(sizeof(char) * size); idx < size; idx++)
-		binary[idx] = !!(size & (1 << idx));
-	for (idx = size - 1, parent = *root; idx > 0; idx--)
-		parent = binary[idx] ? parent->right : parent->left;
-	if (binary[idx])
-		parent->right = node;
-	else
-		parent->left = node;
-	node->parent = parent;
-	free(binary);
-	heapify(root);
+	{
+		*root = node;
+		return (node);
+	}
+	parent_size = binary_tree_size(*root);
+	node_size = 0;
+	while (1)
+	{
+		if (parent_size & node_size)
+		{
+			if ((*root)->left)
+				*root = (*root)->left;
+			else
+			{
+				(*root)->left = node;
+				break;
+			}
+		}
+		else
+		{
+			if ((*root)->right)
+				*root = (*root)->right;
+			else
+			{
+				(*root)->right = node;
+				break;
+			}
+		}
+		node_size >>= 1;
+	}
+	node->parent = *root;
+	node_swap(&node);
 	return (node);
+}
+
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: pointer to the root node of the tree to measure the size
+ * Return: size of a binary tree or 0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
