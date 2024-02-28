4cfe0a86bae4a41ad1a12009341e853ecf7f8b72
Gilt457 <lepatioborel@gmail.com>
2024-02-28 04:40:07 -0800

    update

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -10,33 +10,40 @@ int height(const binary_tree_t *tree)
 	int left_height, right_height;
 
 	if (tree == NULL)
-		return (-1);
+		return (0);
 
 	left_height = height(tree->left);
 	right_height = height(tree->right);
 
-	if (left_height > right_height)
-		return (left_height + 1);
-	return (right_height + 1);
+	return (left_height > right_height ? left_height + 1 : right_height + 1);
 }
 
 /**
- * is_balanced - Checks if a binary tree is balanced
+ * is_avl_helper - Checks if a binary tree is a valid AVL Tree
  * @tree: Pointer to the root node of the tree to check
- * Return: 1 if tree is balanced, and 0 otherwise
+ * @lo: The minimum possible value for nodes in the tree
+ * @hi: The maximum possible value for nodes in the tree
+ * Return: 1 if tree is a valid AVL Tree, and 0 otherwise
  */
-int is_balanced(const binary_tree_t *tree)
+int is_avl_helper(const binary_tree_t *tree, int lo, int hi)
 {
-	int balance_factor;
+	int lhgt, rhgt, diff;
 
 	if (tree == NULL)
 		return (1);
 
-	balance_factor = height(tree->left) - height(tree->right);
-
-	if (balance_factor > 1 || balance_factor < -1)
+	if (tree->n < lo || tree->n > hi)
 		return (0);
-	return (is_balanced(tree->left) && is_balanced(tree->right));
+
+	lhgt = height(tree->left);
+	rhgt = height(tree->right);
+
+	diff = lhgt - rhgt;
+
+	if (diff > 1 || diff < -1)
+		return (0);
+
+	return (is_avl_helper(tree->left, lo, tree->n - 1) && is_avl_helper(tree->right, tree->n + 1, hi));
 }
 
 /**
@@ -49,8 +56,5 @@ int binary_tree_is_avl(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (!binary_tree_is_bst(tree))
-		return (0);
-
-	return (is_balanced(tree));
+	return (is_avl_helper(tree, INT_MIN, INT_MAX));
 }
