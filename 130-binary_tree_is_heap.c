55b1b7d7d93f4e7cd04ddde5bea37df24bb49de1
Gilt457 <lepatioborel@gmail.com>
2024-02-28 08:14:30 -0800

    update

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -1,9 +1,5 @@
 #include "binary_trees.h"
 
-/**
- * This file contains functions to check if a binary tree is a valid Max Binary Heap.
- */
-
 int binary_tree_is_heap(const binary_tree_t *tree);
 int check_parent(const binary_tree_t *tree);
 int binary_tree_is_complete(const binary_tree_t *tree);
@@ -15,7 +11,7 @@ size_t binary_tree_size(const binary_tree_t *tree);
  *
  * @tree: A pointer to the root node of the tree to check.
  *
- * Return: 1 if the tree is a valid Max Binary Heap, and 0 otherwise.
+ * Returns 1 if the tree is a valid Max Binary Heap, and 0 otherwise.
  */
 int binary_tree_is_heap(const binary_tree_t *tree)
 {
@@ -30,7 +26,7 @@ int binary_tree_is_heap(const binary_tree_t *tree)
  *
  * @tree: A pointer to the node to check.
  *
- * Return: 1 if the parent has a greater value, 0 otherwise.
+ * Returns 1 if the parent has a greater value, 0 otherwise.
  */
 int check_parent(const binary_tree_t *tree)
 {
@@ -47,7 +43,7 @@ int check_parent(const binary_tree_t *tree)
  * Calls the tree_is_complete function to determine if a tree is complete.
  *
  * @tree: The root of the tree.
- * Return: 1 if the tree is complete, 0 otherwise.
+ * Returns 1 if the tree is complete, 0 otherwise.
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
@@ -68,7 +64,7 @@ int binary_tree_is_complete(const binary_tree_t *tree)
  * @i: Node index.
  * @cnodes: Number of nodes.
  *
- * Return: 1 if the tree is complete, 0 otherwise.
+ * Returns 1 if the tree is complete, 0 otherwise.
  */
 int tree_is_complete(const binary_tree_t *tree, int i, int cnodes)
 {
@@ -86,7 +82,7 @@ int tree_is_complete(const binary_tree_t *tree, int i, int cnodes)
  * Measures the size of a binary tree.
  *
  * @tree: The root of the tree.
- * Return: The size of the tree or 0 if the tree is NULL.
+ * Returns the size of the tree or 0 if the tree is NULL.
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
